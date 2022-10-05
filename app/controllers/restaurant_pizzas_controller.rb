fcda2fa6f014701771d6c28015107637ba9e16b0
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-05 15:29:58 +0300

    updated RestaurantPizza contoller

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,24 @@
 class RestaurantPizzasController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create
+        restaurant_pizza= RestaurantPizza.create!(restaurant_pizza_params)
+        pizza = Pizza.find(restaurant_pizza.pizza_id)
+        render json: pizza, status: :created
+    end
+
+    private
+
+    def restaurant_pizza_params
+        params.permit(:price, :pizza_id, :restaurant_id)
+    end
+    
+    def render_not_found_response
+        render json: { error: "RestaurantPizza not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+    end
 end
