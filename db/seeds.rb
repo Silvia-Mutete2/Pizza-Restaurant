49c8162469d25fd9833de1a5c9ffd4284b1e1714
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-05 14:05:07 +0300

    Seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,61 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "🌱 Seeding..."
+
+Pizza.create!([
+    {
+        name:  "Cheese",
+        ingredients:  "Dough, Tomato Sauce, Cheese"
+    },
+    {
+        name: "Pepperoni",
+        ingredients: "Dough, Tomato Sauce, Cheese, Pepperoni"
+    },
+    {
+       name: "Marinara",
+       ingredients: "Tomatoes, Garlic, Oregano, Virgin olive oil"
+    },
+    {
+        name: "Margherita",
+        ingredients: "Tomatoes,Mozzarella, Basil,Virgin olive oil"
+    }
+
+])
+
+Restaurant.create!([
+    {
+        name: "Sottocasa NYC",
+        address: "298 Atlantic Ave, Brooklyn, NY 11201"
+    },
+    {
+        name: "PizzArte",
+        address: "69 W 55th St, New York, NY 10019"
+    },
+    {
+        name: "Kempinski",
+        address: " Nairobi, Westlands"
+    },
+    {
+        name: "Serena",
+        address: "Nairobi, NY 10019"
+    }
+
+])
+
+RestaurantPizza.create!([
+    {
+        price: 5,
+        pizza_id: 1,
+        restaurant_id: 3
+    },
+    {
+        price: 10,
+        pizza_id: 4,
+        restaurant_id: 3
+    },
+    {
+        price: 7,
+        pizza_id: 2,
+        restaurant_id: 4
+    }
+])
+
+puts "✅ Done seeding!"
