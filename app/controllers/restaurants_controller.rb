67ee898d38031d21c8542374ede3915c871b5356
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-05 14:53:06 +0300

    Handled restaurant delete and error function

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,12 +1,31 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
     def index 
         restaurants = Restaurant.all 
-        render json: restaurants, status: :ok
+        render json: restaurants, status: :created
     end
 
     def show
         restaurant = Restaurant.find(params[:id])
-        render json: restaurant, status: :ok
+        render json: restaurant, status: :created
     end
+
+    def destroy 
+        restaurant = find_restaurant
+        restaurant.destroy
+        head :no_content
+    end
+
+    private
+
+    def find_plant
+        Restaurant.find(params[:id])
+    end
+    
+
+    def render_not_found_response
+        render json: { error: "Restaurant not found" }, status: :not_found
+    end
+
 end
