4d84dfba72662fe01ed79128b708f48e165a7003
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-05 14:46:01 +0300

    updated restaurant controller and routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,12 @@
 class RestaurantsController < ApplicationController
+
+    def index 
+        restaurants = Restaurant.all 
+        render json: restaurants, status: :ok
+    end
+
+    def show
+        restaurant = Restaurant.find(params[:id])
+        render json: restaurant, status: :ok
+    end
 end
